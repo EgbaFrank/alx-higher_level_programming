0e9bf903ab095f215b4830707d6f461ab96158de
Egba Frank <egbafrank@gmail.com>
2024-03-14 20:48:52 -0700

    Added task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4, dis
+    import hidden_4
 
-    dis.dis()
+    for name in dir(hidden_4):
+        if '__' not in name:
+            print(name)
