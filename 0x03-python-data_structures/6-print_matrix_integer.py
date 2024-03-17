c34c81c88198e3981b1bf023bd8a1e283fba865e
Egba Frank <Egbafrank72@gmail.com>
2024-03-17 22:20:57 +0000

    Added bugfix, task 3, 6, 7

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        count = 1
-
-        for i in row:
-            if count < 3:
-                print("{:d} ".format(i), end='')
-            else:
-                print("{:d}".format(i), end='')
-            count += 1
-        print()
+        print(" ".join("{:d}".format(i) for i in row))
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = list(tuple_a)
-    tuple_b = list(tuple_b)
-    tuple_a.extend([0, 0])
-    tuple_b.extend([0, 0])
+    # calculate tuple lenght
+    a_len = len(tuple_a)
+    b_len = len(tuple_b)
+    
+    # checks if tuple_b lenght is less than 2
+    if a_len == 0:
+        tuple_a = (0, 0)
+    elif a_len == 1:
+        tuple_a = (tuple_a[0], 0)
 
-    result = [tuple_a[i] + tuple_b[i] for i in range(2)]
-    return tuple(result)
+    # checks if tuple_b lenght is less than 2
+    if b_len == 0:
+        tuple_b = (0, 0)
+    elif b_len == 1:
+        tuple_b = (tuple_b[0], 0)
+
+    # calculate and return result
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
