0aa2078843a07cff785a66d61c988638e6ac9581
Egba Frank <Egbafrank72@gmail.com>
2024-03-20 20:39:48 +0000

    Added bugfix, -task 4, 7, 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new = {key: value * 2 for key, value in a_dictionary}
-    return new
+    return {key: value * 2 for key, value in a_dictionary.items()}
