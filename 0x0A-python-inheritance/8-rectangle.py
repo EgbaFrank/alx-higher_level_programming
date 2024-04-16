3c032e0107fc431f5133057beaf3c6cb17dcf67c
Egba Frank <Egbafrank72@gmail.com>
2024-04-16 08:44:42 +0000

    Added bugfix updates

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,39 +4,8 @@ This module contains a BaseGeometry class
 """
 
 
-class BaseGeometry:
-    """
-    Creates a Geometry blueprint
-    """
-    def area(self):
-        """
-        Should output the geometry's area
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-        Returns:
-            int / float: The geometry's area
-
-        Raises:
-            Exception: That is being worked on currently
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates the value entered
-
-        Args:
-            name (str): Name of perhaps the shape
-            value (int): value to be validated
-
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value if less than 1
-        """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-
-        if value < 1:
-            raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
@@ -51,7 +20,7 @@ class Rectangle(BaseGeometry):
             width (int): rectangle's width size
             height (int): rectangle's height size
         """
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
