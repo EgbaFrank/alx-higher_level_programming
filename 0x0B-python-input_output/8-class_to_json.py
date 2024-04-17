7dc1f68553d63e2d810640600299abe09ca3d9bc
Egba Frank <Egbafrank72@gmail.com>
2024-04-17 22:29:40 +0000

    Added more mandatory tasks

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains JSON funtionality
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description for an object JSON serialization
+
+    Args:
+        obj: object to be applied on
+
+    Returns:
+        dict: description JSON serialization with simple data structures
+    """
+    return obj.__dict__
