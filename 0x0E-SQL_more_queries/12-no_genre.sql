d46431e1edbfd1eb4d6b68bdd09766669f79dcdd
Egba Frank <Egbafrank72@gmail.com>
2024-05-24 09:39:07 +0000

    Added more mandatory tasks

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,10 +1,7 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-NATURAL JOIN tv_show_genres
-WHERE tv_shows.title = NULL
-
-
-
-
-ORDER BY tv_shows.title, tv_show_genres.genre_id; 
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title; 
