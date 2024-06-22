2977e07d6541f4dd331cbbcc99732d732a2c5352
Egba Frank <Egbafrank72@gmail.com>
2024-06-22 00:14:45 +0000

    Added quick-fix, -task 1

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,28 +7,23 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    try:
-        db = MySQLdb.connect(
-                    port=3306,
-                    host="localhost",
-                    user=sys.argv[1],
-                    passwd=sys.argv[2],
-                    db=sys.argv[3]
-        )
-        cur = db.cursor()
+    db = MySQLdb.connect(
+                port=3306,
+                host="localhost",
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
+    )
+    cur = db.cursor()
 
-        cur.execute("SELECT *\
-                     FROM states\
-                     WHERE name = '{}'\
-                     ORDER BY id ASC".format(sys.argv[4]))
-        states = cur.fetchall()
+    cur.execute("SELECT *\
+                 FROM states\
+                 WHERE name LIKE BINARY '{}'\
+                 ORDER BY id ASC".format(sys.argv[4]))
+    states = cur.fetchall()
 
-        for row in states:
-            print(row)
+    for row in states:
+        print(row)
 
-    except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL: {e}")
-
-    finally:
-        cur.close()
-        db.close()
+    cur.close()
+    db.close()
