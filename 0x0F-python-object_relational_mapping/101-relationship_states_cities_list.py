b63e79f1bc64e13771570d99471ec9db25acf22b
Egba Frank <Egbafrank72@gmail.com>
2024-06-23 23:09:12 +0000

    Updated query efficiency 2

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -11,23 +11,15 @@ from sqlalchemy.orm import sessionmaker, joinedload
 
 
 if __name__ == "__main__":
-    # Usage check
-    if len(sys.argv) != 4:
-        print(
-                "Usage: {} <mysql username> <mysql password> <database name>"
-                .format(sys.argv[0])
-                )
-        sys.exit(1)
+    """Create the engine and bind it to the session"""
 
-    # Create the engine and bind it to the session
     engine = create_engine(
             'mysql+mysqldb://{}:{}@localhost/{}'
             .format(sys.argv[1],
                     sys.argv[2],
                     sys.argv[3]
                     ),
-            pool_pre_ping=True,
-            echo=True
+            pool_pre_ping=True
             )
 
     Session = sessionmaker(bind=engine)
