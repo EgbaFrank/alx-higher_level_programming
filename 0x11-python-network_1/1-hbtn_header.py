56c328dc704d79fa79d386d5860e90e77c1676e5
Egba Frank <Egbafrank72@gmail.com>
2024-07-05 22:31:30 +0000

    Added some more bugfixes

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     with urlopen(url) as response:
-        print(response.headers().get('X-Request-Id'))
+        print(response.info().get('X-Request-Id'))
