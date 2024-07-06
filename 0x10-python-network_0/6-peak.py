80e856fccc2a5e8f994a5d27c26cc820a1568bfb
Egba Frank <Egbafrank72@gmail.com>
2024-07-06 18:40:08 +0000

    Added task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    n = len(list_of_integers)
+
+    if n == 1:
+        return list_of_integers[0]
+
+    if list_of_integers[0] >= list_of_integers[1]:
+        return list_of_integers[0]
+
+    if list_of_integers[-1] >= list_of_integers[-2]:
+        return list_of_integers[-1]
+
+    left, right = 0, n - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if ((mid == 0 or
+                list_of_integers[mid] >= list_of_integers[mid - 1]) and
+        (mid == n - 1 or
+                list_of_integers[mid] >= list_of_integers[mid + 1])):
+            return list_of_integers[mid]
+
+        elif ((mid > 0) and
+                (list_of_integers[mid] < list_of_integers[mid - 1])):
+            right = mid - 1
+
+        else:
+            left = mid + 1
+
+    return None
