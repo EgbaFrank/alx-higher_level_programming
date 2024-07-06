b9fb307f084fade692a35bd5fd60142d471b79b9
Egba Frank <Egbafrank72@gmail.com>
2024-07-06 20:15:32 +0000

    Fixed script lenght

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Displays only the response status code of a specified URL
-
-if [ $# -ne 1 ]; then
-	echo "Usage: '$0' [URL]"
-	exit 1
-fi
-
 curl -o /dev/null -sw '%{http_code}\n' "$1"
