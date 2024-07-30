3a5f6239e891c88f1dccbb419677449cd34df915
Egba Frank <Egbafrank72@gmail.com>
2024-07-30 23:43:05 +0000

    Added command line input

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints the number of movies with the character “Wedge Antilles”
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/';
+const url = process.argv[2];
 const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(url, (error, response, body) => {
