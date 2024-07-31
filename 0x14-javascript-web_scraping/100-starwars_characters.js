b98b1d9b858711265b721bc05b6eb94e9b8e7882
Egba Frank <Egbafrank72@gmail.com>
2024-07-31 07:23:36 +0000

    Added semistandard compliance

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,23 +2,23 @@
 // Prints all characters of a Star Wars movie
 const request = require('request');
 const ID = process.argv[2];
-const film_url = `https://swapi-api.alx-tools.com/api/films/${ID}/`;
+const url = `https://swapi-api.alx-tools.com/api/films/${ID}/`;
 
-request(film_url, (error, response, body) => {
-	if (error) {
-		console.error(error);
-		return;
-	}
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-	const characters = JSON.parse(body).characters
-	for (const character of characters) {
-		request(character, (err, response, body) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
-			result = JSON.parse(body).name;
-			console.log(result);
-		});
-	}
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    request(character, (err, response, body) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const result = JSON.parse(body).name;
+      console.log(result);
+    });
+  }
 });
